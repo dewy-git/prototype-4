50ece98801067bffceaf7974df1d888cd6e918b5
dewy-git <dewu@s.sfusd.edu>
2023-03-03 15:44:22 -0800

    completed unit 4.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float speed = 5.0f;
     private GameObject focalPoint;
+    private float powerupStrength = 15.0f;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +25,36 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+       {
+        Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+        Debug.Log("Collided with" + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        enemyRigidBody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+       } 
     }
 }
